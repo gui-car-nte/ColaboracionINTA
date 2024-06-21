167811d58be23a85a92c062dc23c49916c893fe8
Miguel Ángel Durán <miguel.duran@nter.es>
2024-06-21 15:25:49 +0200

    GUI-1: complete

--- inta/src/front/gui.py
+++ inta/src/front/gui.py
@@ -6,56 +6,57 @@ from src.back.file_handler import FileHandler
 
 def start_gui():
 
-    color_primary = "#E7EFE7"
-    color_secundary = "#1F62B1"
+    color_primary = "#E7EFE7" # Blanco
+    color_secundary = "#1F62B1" # Azul
 
     def load_files():
 
-        c = Calculations()
-        f = FileHandler()
+            c = Calculations()
+            f = FileHandler()
 
-        filepaths = filedialog.askopenfilenames(filetypes=[("CSV files", "*.csv")])
-        sensor_data = f.load_csv_files(filepaths)
-        result = c.calculate_magnetic_moment(sensor_data)
-        result_label.config(text=str(result))
-
-    def distance_dialog():
-        pass
+            filepaths = filedialog.askopenfilenames(filetypes=[("CSV files", "*.csv")])
+            sensor_data = f.load_csv_files(filepaths)
+            result = c.calculate_magnetic_moment(sensor_data)
+            result_label.config(text=str(result))
 
     root = tk.Tk()
     root.title("Cálculo del Momento Magnético")
     root.configure(background=color_primary)
-    
-    main_frame = tk.Frame(root)
-    main_frame.configure(background=color_primary, )
-    main_frame.pack()
 
-    button_frame = tk.Frame(main_frame)
-    button_frame.configure(background=color_primary)
-    button_frame.pack()
-    
-    result_frame = tk.Frame(main_frame)
-    result_frame.configure(background=color_primary)
-    result_frame.pack()
+    top_frame = tk.Frame(root)
+    top_frame.configure(background=color_primary)
+    top_frame.pack(side=tk.TOP, pady=10)
 
-    sumary_frame = tk.Frame(main_frame)
-    sumary_frame.configure(background=color_primary)
-    sumary_frame.pack()
+    button1 = tk.Button(top_frame, text="Cargar Archivos CSV", command=load_files, background=color_secundary)
+    button1.pack(side=tk.LEFT, padx=10)
 
-    load_button = tk.Button(button_frame, text="Cargar Archivos CSV", command=load_files, background=color_secundary)
-    load_button.pack(side=tk.LEFT)
+    button2 = tk.Button(top_frame, text="Introducir distancias", background=color_secundary)
+    button2.pack(side=tk.LEFT, padx=10)
 
-    distance_button = tk.Button(button_frame, text="Introducir distancias", background=color_secundary)
-    distance_button.pack(side=tk.LEFT)
-    
-    result_label = tk.Label(result_frame, text="Resultados aparecerán aquí", background=color_primary)
-    result_label.pack()
+    button3 = tk.Button(top_frame, text="Exportar a PDF", background=color_secundary)
+    button3.pack(side=tk.LEFT, padx=10)
 
-    x_label = tk.Label(sumary_frame, text="X:", background=color_primary)
-    y_label = tk.Label(sumary_frame, text="Y:", background=color_primary)
-    z_label = tk.Label(sumary_frame, text="Z:", background=color_primary)
-    x_label.pack(side=tk.LEFT)
-    y_label.pack(side=tk.LEFT)
-    z_label.pack(side=tk.LEFT)
-    
+    center_frame = tk.Frame(root)
+    # center_frame.configure(background=color_primary)
+    center_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True, pady=10)
+
+    result_label = tk.Label(center_frame, text="", wraplength=300, justify=tk.LEFT)
+    result_label.pack(pady=20)
+
+    bottom_frame = tk.Frame(root)
+    bottom_frame.configure(background=color_primary)
+    bottom_frame.pack(side=tk.BOTTOM, pady=10)
+
+    x_label = tk.Label(bottom_frame, text="X:")
+    x_label.configure(background=color_primary)
+    x_label.pack(side=tk.LEFT, padx=10)
+
+    y_label = tk.Label(bottom_frame, text="Y:")
+    y_label.configure(background=color_primary)
+    y_label.pack(side=tk.LEFT, padx=10)
+
+    z_label = tk.Label(bottom_frame, text="Z:")
+    z_label.configure(background=color_primary)
+    z_label.pack(side=tk.LEFT, padx=10)
+
     root.mainloop()
