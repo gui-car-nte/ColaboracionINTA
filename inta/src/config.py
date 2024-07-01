8b4802bff4fd30ec1879d785dc79a5c336e56d66
Miguel Ángel Durán <miguel.duran@nter.es>
2024-07-01 09:08:21 +0200

    Feature/FS-4: create pdf file

--- inta/src/config.py
+++ inta/src/config.py
@@ -4,3 +4,5 @@ FINAL_MOMENTUM = 1000
 PRECISION = 15
 PRIMARY_COLOR = "#E7EFE7"
 SECONDARY_COLOR = "#1F62B1"
+RUTA_IMAGEN = 'src/front/resource/imagen.png'
+ICON_RUTE = 'src/front/resource/logo.ico'
--- inta/src/front/gui.py
+++ inta/src/front/gui.py
@@ -1,18 +1,22 @@
 import tkinter as tk
 
-from src.front.tkinter_settings import GuiServices, COLOR_PRIMARY, COLOR_SECUNDARY
+from src.front.tkinter_settings import GuiServices
+from src import config
 
 def start_gui():
     root = tk.Tk()
     root.title("Cálculo del Momento Magnético")
     root.minsize(600, 400)
-    root.configure(background=COLOR_PRIMARY)
+    photo = tk.PhotoImage(file = 'src/front/resource/logo.png')
+    root.wm_iconphoto(False, photo)
+    # root.iconbitmap('src/front/resource/logo.ico')
+    root.configure(background=config.PRIMARY_COLOR)
     
     settings = GuiServices(root)
 
     top_frame = settings.create_frame(root, tk.TOP)
     center_frame = settings.create_frame(root, tk.TOP, complete=True, scrollable=True)
-    settings.create_image_canvas(center_frame,'src/front/resource/logo.png').configure(background=COLOR_PRIMARY)
+    settings.create_image_canvas(center_frame,'src/front/resource/logo.png').configure(background=config.PRIMARY_COLOR)
     bottom_frame = settings.create_frame(root, tk.BOTTOM)
 
     settings.create_button("csv_button", top_frame, "Cargar Archivos CSV", tk.LEFT, settings.load_files, center_frame)
