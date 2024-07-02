e0acf0790d43c737e98504aa1e03616b711725fa
Miguel Ángel Durán <miguel.duran@nter.es>
2024-07-02 17:58:37 +0200

    FEATURE/GUI-9: separete files

--- inta/src/front/tkinter_settings.py
+++ inta/src/front/tkinter_settings.py
@@ -3,7 +3,6 @@ import os
 import logging
 
 from tkinter import filedialog
-from PIL import Image, ImageTk
 
 from src.back.calculations import Calculations
 from src.back.file_handler import FileHandler
@@ -11,6 +10,7 @@ from src import config
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 from reportlab.lib.utils import ImageReader
+from src.front.utils import Utils
 
 # Configura el logger al inicio de tu aplicación
 logging.basicConfig(
@@ -36,6 +36,7 @@ class GuiServices:
         self.sensor_data = ""
         self.message_label = tk.Label(frame_main, text="", bg=config.PRIMARY_COLOR)
         self.message_label.pack(pady=10)
+        self.utils = Utils()
 
     def load_files(self, next_frame):
         filepaths = filedialog.askopenfilenames(filetypes=[("CSV files", "*.csv")])
@@ -46,92 +47,20 @@ class GuiServices:
         self.drop_frame(next_frame)
         self.input_distance(next_frame)
 
-    def create_frame(self, frame_in, side_in, complete=False, scrollable=False):
-        frame = tk.Frame(frame_in)
-        frame.configure(background=config.PRIMARY_COLOR)
-
-        if complete:
-            frame.pack(side = side_in, fill =  tk.BOTH, expand = True, pady = 10)
-        else:
-            frame.pack(side=side_in, pady=10)
-
-        if scrollable:
-            frame = self.create_scroll(frame)
-            
-        return frame
-
-    def on_frame_configure(self, canvas):
-        canvas.configure(scrollregion=canvas.bbox("all"))
-
-    def _on_mouse_wheel(self, event, canvas):
-        canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
-
-    def create_button(
-        self, name_in, frame_in, text_in, side_in, command_in, parameter_in
-    ):
-        if parameter_in != "":
-            button = tk.Button(
-                frame_in,
-                text=text_in,
-                background=config.SECONDARY_COLOR,
-                name=name_in,
-                command=lambda: command_in(parameter_in),
-            )
-            button.configure(fg="white")
-            if side_in == "":
-                button.pack(pady=10, anchor=tk.NW)
-            else:
-                button.pack(side=side_in, pady=10, anchor=tk.NW)
-        else:
-            button = tk.Button(
-                frame_in,
-                text=text_in,
-                background=config.SECONDARY_COLOR,
-                name=name_in,
-                command=command_in,
-            )
-            button.configure(fg="white")
-            if side_in == "":
-                button.pack(pady=10)
-            else:
-                button.pack(side=side_in, pady=10)
-
-        return button
-
-    def create_label(self, frame_in, text_in, side_in, diff=False):
-        if diff:
-            result_label = tk.Label(
-                frame_in, text=text_in, wraplength=300, justify=side_in
-            )
-            result_label.pack(pady=20)
-        else:
-            label = tk.Label(frame_in, text=text_in)
-            label.configure(background=config.PRIMARY_COLOR)
-            if side_in == "":
-                label.pack()
-            else:
-                label.pack(side=side_in)
-                
-        return label
-
-    def create_input(self, frame_in):
-        entry = tk.Entry(frame_in)
-        entry.pack()
-
     def input_distance(self, next_frame):
         for sensor in range(self.numbers_sensors):
-            frame_input = self.create_frame(next_frame, tk.TOP, False)
-            self.create_label(frame_input, f"Sensor Distance {sensor + 1} ", tk.LEFT)
-            self.create_input(frame_input)
+            frame_input = self.utils.create_frame(next_frame, tk.TOP, False)
+            self.utils.create_label(frame_input, f"Sensor Distance {sensor + 1} ", tk.LEFT)
+            self.utils.create_input(frame_input)
 
-        self.create_button(
+        self.utils.create_button(
             "calculate", next_frame, "Calculate", "", self.send_distance, next_frame
         )
 
     def send_distance(self, frame_main: tk.Frame):
-        distances = self.recoger_valores(frame_main)
+        distances = self.get_values(frame_main)
         self.drop_frame(frame_main)
-        new_frame = self.create_frame(frame_main, tk.TOP, False)
+        new_frame = self.utils.create_frame(frame_main, tk.TOP, False)
         self.create_result(new_frame, distances)
 
     def drop_frame(self, frame_main):
@@ -141,13 +70,13 @@ class GuiServices:
             widget.destroy()
 
     # TODO change name & var names
-    def recoger_valores(self, frame):
+    def get_values(self, frame):
         valores = []
         for widget in frame.winfo_children():
             if isinstance(widget, tk.Entry):
                 valores.append(widget.get())
             elif isinstance(widget, tk.Frame):
-                valores.extend(self.recoger_valores(widget))
+                valores.extend(self.get_values(widget))
                 
         return valores
 
@@ -156,67 +85,10 @@ class GuiServices:
 
         self.results = c.calculate_magnetic_moment(self.sensor_data, distances)
         for result, image_path in zip(self.results, config.IMAGES):
-            self.create_image_canvas(frame_main, image_path)
-            self.create_label(frame_main, f"Magentic Moment: {result}", "").configure(
+            self.utils.create_image_canvas(frame_main, image_path)
+            self.utils.create_label(frame_main, f"Magentic Moment: {result}", "").configure(
                 padx=10, pady=10
             )
-
-    def create_image_canvas(self, frame, image_path=""):
-        if image_path == "":
-            image = Image.open(image_path)
-            photo = ImageTk.PhotoImage(image)
-
-            canvas = tk.Canvas(frame, width=image.width, height=image.height)
-            canvas.pack(pady=10)
-            canvas.create_image(0, 0, anchor="nw", image=photo)
-
-            # Keep a reference to the image to prevent garbage collection # TODO research garbage collector
-            canvas.image = photo
-        else:
-            image = Image.open(image_path)
-            photo = ImageTk.PhotoImage(image)
-
-            canvas = tk.Canvas(frame, width=image.width, height=image.height)
-            canvas.pack(pady=10)
-            canvas.create_image(0, 0, anchor="nw", image=photo)
-
-            # Keep a reference to the image to prevent garbage collection # TODO research garbage collector
-            canvas.image = photo
-            
-        return canvas
-
-    # TODO could be moved to utils folder or another file to shorten code, go case by case
-    # TODO '=' spacing
-    def create_scroll(self, frame_main):
-        # Crear el lienzo
-        canvas = tk.Canvas(frame_main, bg = config.PRIMARY_COLOR)
-        canvas.pack(side=tk.LEFT, fill = tk.BOTH, expand = True)
-
-        # Crear la barra de desplazamiento
-        scrollbar_y = tk.Scrollbar(frame_main, orient = tk.VERTICAL, command = canvas.yview)
-        scrollbar_y.pack(side = tk.RIGHT, fill = tk.Y)
-        canvas.configure(yscrollcommand = scrollbar_y.set)
-
-        # Crear un marco dentro del lienzo
-        inner_frame = tk.Frame(canvas, bg = config.PRIMARY_COLOR)
-        window = canvas.create_window((0, 0), window=inner_frame, anchor = "n")
-
-        def center_frame(event):
-            canvas_width = event.width
-            canvas.coords(window, canvas_width // 2, 0)
-            canvas.configure(scrollregion=canvas.bbox("all"))
-
-        canvas.bind("<Configure>", center_frame)
-        inner_frame.bind(
-            "<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all"))
-        )
-
-        # Evento de rueda del ratón para desplazar
-        canvas.bind_all(
-            "<MouseWheel>", lambda event: self._on_mouse_wheel(event, canvas)
-        )
-
-        return inner_frame
 
     # TODO make 'c' variable more descriptive & english tl
     def export_to_pdf(self):
@@ -255,7 +127,7 @@ class GuiServices:
 
     def show_message(self, msg, color):
         if self.message_label:
-            self.message_label.config(text=msg, fg=color, background=config.PRIMARY_COLOR)
+            self.message_label.config(text = msg, fg = color, background = config.PRIMARY_COLOR)
             self.window.after(5000, self.clear_message)
         else:
             print("Message label not defined.")
@@ -267,14 +139,4 @@ class GuiServices:
     def log_error(self, error_type, error_message):
         logger.error(f'{error_type}: {error_message}')
         self.show_message(f'{error_type}: {error_message}', 'red')
-        
-    def some_function(self):
-        try:
-            # Código que puede generar una excepción
-            raise ValueError("This is a value error example")
-        except ValueError as e:
-            self.log_error('ValueError', str(e))
-        except TypeError as e:
-            self.log_error('TypeError', str(e))
-        except Exception as e:
-            self.log_error('Exception', str(e))
+    
---
+++ inta/src/front/utils.py
@@ -0,0 +1,135 @@
+import tkinter as tk
+
+from src import config
+from PIL import Image, ImageTk
+
+
+class Utils():
+    def create_frame(self, frame_in, side_in, complete = False, scrollable = False):
+        frame = tk.Frame(frame_in)
+        frame.configure(background = config.PRIMARY_COLOR)
+
+        if complete:
+            frame.pack(side = side_in, fill =  tk.BOTH, expand = True, pady = 10)
+        else:
+            frame.pack(side = side_in, pady = 10)
+
+        if scrollable:
+            frame = self.create_scroll(frame)
+            
+        return frame
+
+    def on_frame_configure(self, canvas):
+        canvas.configure(scrollregion = canvas.bbox("all"))
+
+    def _on_mouse_wheel(self, event, canvas):
+        canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
+
+    def create_button(
+        self, name_in, frame_in, text_in, side_in, command_in, parameter_in
+    ):
+        if parameter_in != "":
+            button = tk.Button(
+                frame_in,
+                text = text_in,
+                background = config.SECONDARY_COLOR,
+                name = name_in,
+                command = lambda: command_in(parameter_in),
+            )
+            button.configure(fg="white")
+            if side_in == "":
+                button.pack(pady = 10, anchor = tk.NW)
+            else:
+                button.pack(side=side_in, pady = 10, anchor = tk.NW)
+        else:
+            button = tk.Button(
+                frame_in,
+                text = text_in,
+                background = config.SECONDARY_COLOR,
+                name = name_in,
+                command = command_in,
+            )
+            button.configure(fg="white")
+            if side_in == "":
+                button.pack(pady = 10)
+            else:
+                button.pack(side = side_in, pady = 10)
+
+        return button
+
+    def create_label(self, frame_in, text_in, side_in, diff = False):
+        if diff:
+            result_label = tk.Label(
+                frame_in, text = text_in, wraplength = 300, justify = side_in
+            )
+            result_label.pack(pady=20)
+        else:
+            label = tk.Label(frame_in, text = text_in)
+            label.configure(background = config.PRIMARY_COLOR)
+            if side_in == "":
+                label.pack()
+            else:
+                label.pack(side = side_in)
+                
+        return label
+
+    def create_input(self, frame_in):
+        entry = tk.Entry(frame_in)
+        entry.pack()
+
+    def create_image_canvas(self, frame, image_path = ""):
+        if image_path == "":
+            image = Image.open(image_path)
+            photo = ImageTk.PhotoImage(image)
+
+            canvas = tk.Canvas(frame, width = image.width, height = image.height)
+            canvas.pack(pady = 10)
+            canvas.create_image(0, 0, anchor = "nw", image = photo)
+
+            # Keep a reference to the image to prevent garbage collection # TODO research garbage collector
+            # canvas.image = photo
+        else:
+            image = Image.open(image_path)
+            photo = ImageTk.PhotoImage(image)
+
+            canvas = tk.Canvas(frame, width = image.width, height = image.height)
+            canvas.pack(pady = 10)
+            canvas.create_image(0, 0, anchor = "nw", image = photo)
+
+            # Keep a reference to the image to prevent garbage collection # TODO research garbage collector
+            # canvas.image = photo
+            
+        return canvas
+
+    # TODO could be moved to utils folder or another file to shorten code, go case by case
+    # TODO '=' spacing
+    def create_scroll(self, frame_main):
+        # Crear el lienzo
+        canvas = tk.Canvas(frame_main, bg = config.PRIMARY_COLOR)
+        canvas.pack(side=tk.LEFT, fill = tk.BOTH, expand = True)
+
+        # Crear la barra de desplazamiento
+        scrollbar_y = tk.Scrollbar(frame_main, orient = tk.VERTICAL, command = canvas.yview)
+        scrollbar_y.pack(side = tk.RIGHT, fill = tk.Y)
+        canvas.configure(yscrollcommand = scrollbar_y.set)
+
+        # Crear un marco dentro del lienzo
+        inner_frame = tk.Frame(canvas, bg = config.PRIMARY_COLOR)
+        window = canvas.create_window((0, 0), window = inner_frame, anchor = "n")
+
+        def center_frame(event):
+            canvas_width = event.width
+            canvas.coords(window, canvas_width // 2, 0)
+            canvas.configure(scrollregion=canvas.bbox("all"))
+
+        canvas.bind("<Configure>", center_frame)
+        inner_frame.bind(
+            "<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all"))
+        )
+
+        # Evento de rueda del ratón para desplazar
+        canvas.bind_all(
+            "<MouseWheel>", lambda event: self._on_mouse_wheel(event, canvas)
+        )
+
+        return inner_frame
