a5e0f6d045ede7c319d0b34e7a9bf6559408fd1f
Miguel Ángel Durán <miguel.duran@nter.es>
2024-07-02 11:45:44 +0200

    FIX/GUI-1: separete equals

--- inta/src/front/tkinter_settings.py
+++ inta/src/front/tkinter_settings.py
@@ -11,10 +11,6 @@ from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 from reportlab.lib.utils import ImageReader
 
-COLOR_PRIMARY = "#E7EFE7"  # Blanco
-COLOR_SECUNDARY = "#1F62B1"  # Azul
-RUTA_IMAGEN = "src/front/resource/imagen.png"
-
 # TODO
 # poner icono en el center_frame para no estar vacio
 # poner iconos en el programa
@@ -41,12 +37,12 @@ class GuiServices:
 
     def create_frame(self, frame_in, side_in, complete=False, scrollable=False):
         frame = tk.Frame(frame_in)
-        frame.configure(background=COLOR_PRIMARY)
+        frame.configure(background = config.PRIMARY_COLOR)
 
         if complete:
-            frame.pack(side=side_in, fill=tk.BOTH, expand=True, pady=10)
+            frame.pack(side = side_in, fill = tk.BOTH, expand = True, pady = 10)
         else:
-            frame.pack(side=side_in, pady=10)
+            frame.pack(side = side_in, pady = 10)
 
         if scrollable:
             frame = self.create_scroll(frame)
@@ -54,7 +50,7 @@ class GuiServices:
         return frame
 
     def on_frame_configure(self, canvas):
-        canvas.configure(scrollregion=canvas.bbox("all"))
+        canvas.configure(scrollregion = canvas.bbox("all"))
 
     def _on_mouse_wheel(self, event, canvas):
         canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
@@ -65,45 +61,45 @@ class GuiServices:
         if parameter_in != "":
             button = tk.Button(
                 frame_in,
-                text=text_in,
-                background=COLOR_SECUNDARY,
-                name=name_in,
-                command=lambda: command_in(parameter_in),
+                text = text_in,
+                background = config.SECONDARY_COLOR,
+                name = name_in,
+                command = lambda: command_in(parameter_in),
             )
-            button.configure(fg="white")
+            button.configure(fg = "white")
             if side_in == "":
-                button.pack(pady=10, anchor=tk.NW)
+                button.pack(pady = 10, anchor = tk.NW)
             else:
-                button.pack(side=side_in, pady=10, anchor=tk.NW)
+                button.pack(side = side_in, pady = 10, anchor = tk.NW)
         else:
             button = tk.Button(
                 frame_in,
-                text=text_in,
-                background=COLOR_SECUNDARY,
-                name=name_in,
-                command=command_in,
+                text = text_in,
+                background = config.SECONDARY_COLOR,
+                name = name_in,
+                command = command_in,
             )
-            button.configure(fg="white")
+            button.configure(fg = "white")
             if side_in == "":
-                button.pack(pady=10)
+                button.pack(pady = 10)
             else:
-                button.pack(side=side_in, pady=10)
+                button.pack(side = side_in, pady = 10)
 
         return button
 
-    def create_label(self, frame_in, text_in, side_in, diff=False):
+    def create_label(self, frame_in, text_in, side_in, diff = False):
         if diff:
             result_label = tk.Label(
-                frame_in, text=text_in, wraplength=300, justify=side_in
+                frame_in, text = text_in, wraplength = 300, justify = side_in
             )
-            result_label.pack(pady=20)
+            result_label.pack(pady = 20)
         else:
-            label = tk.Label(frame_in, text=text_in)
-            label.configure(background=COLOR_PRIMARY)
+            label = tk.Label(frame_in, text = text_in)
+            label.configure(background = config.PRIMARY_COLOR)
             if side_in == "":
                 label.pack()
             else:
-                label.pack(side=side_in)
+                label.pack(side = side_in)
                 
         return label
 
@@ -151,7 +147,7 @@ class GuiServices:
         for result, image_path in zip(self.results, config.IMAGES):
             self.create_image_canvas(frame_main, image_path)
             self.create_label(frame_main, f"Momento Magnetico: {result}", "").configure(
-                padx=10, pady=10
+                padx = 10, pady = 10
             )
 
     def create_image_canvas(self, frame, image_path=""):
@@ -159,9 +155,9 @@ class GuiServices:
             image = Image.open(image)
             photo = ImageTk.PhotoImage(image)
 
-            canvas = tk.Canvas(frame, width=image.width, height=image.height)
+            canvas = tk.Canvas(frame, width = image.width, height = image.height)
             canvas.pack(pady=10)
-            canvas.create_image(0, 0, anchor="nw", image=photo)
+            canvas.create_image(0, 0, anchor = "nw", image = photo)
 
             # Keep a reference to the image to prevent garbage collection # TODO research garbage collector
             canvas.image = photo
@@ -169,9 +165,9 @@ class GuiServices:
             image = Image.open(image_path)
             photo = ImageTk.PhotoImage(image)
 
-            canvas = tk.Canvas(frame, width=image.width, height=image.height)
-            canvas.pack(pady=10)
-            canvas.create_image(0, 0, anchor="nw", image=photo)
+            canvas = tk.Canvas(frame, width = image.width, height = image.height)
+            canvas.pack(pady = 10)
+            canvas.create_image(0, 0, anchor = "nw", image = photo)
 
             # Keep a reference to the image to prevent garbage collection # TODO research garbage collector
             canvas.image = photo
@@ -182,7 +178,7 @@ class GuiServices:
     # TODO '=' spacing
     def create_scroll(self, frame_main):
         # Crear el lienzo
-        canvas = tk.Canvas(frame_main, bg = COLOR_PRIMARY)
+        canvas = tk.Canvas(frame_main, bg = config.PRIMARY_COLOR)
         canvas.pack(side=tk.LEFT, fill = tk.BOTH, expand = True)
 
         # Crear la barra de desplazamiento
@@ -191,8 +187,8 @@ class GuiServices:
         canvas.configure(yscrollcommand = scrollbar_y.set)
 
         # Crear un marco dentro del lienzo
-        inner_frame = tk.Frame(canvas, bg = COLOR_PRIMARY)
-        window = canvas.create_window((0, 0), window=inner_frame, anchor="n")
+        inner_frame = tk.Frame(canvas, bg = config.PRIMARY_COLOR)
+        window = canvas.create_window((0, 0), window = inner_frame, anchor = "n")
 
         def center_frame(event):
             canvas_width = event.width
@@ -201,7 +197,7 @@ class GuiServices:
 
         canvas.bind("<Configure>", center_frame)
         inner_frame.bind(
-            "<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all"))
+            "<Configure>", lambda e: canvas.configure(scrollregion = canvas.bbox("all"))
         )
 
         # Evento de rueda del ratón para desplazar
