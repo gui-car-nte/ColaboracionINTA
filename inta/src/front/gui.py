19b7c816f115b57da50a3ef5085c34282f808128
Miguel Ángel Durán <miguel.duran@nter.es>
2024-07-02 09:29:52 +0200

    FEATURE/FS-5: create function with logging librarie

--- inta/src/front/gui.py
+++ inta/src/front/gui.py
@@ -1,29 +1,43 @@
 import tkinter as tk
+import logging
 
 # TODO review non-used variables
 # TODO english translation
-from src.front.tkinter_settings import GuiServices, COLOR_PRIMARY, COLOR_SECUNDARY
+from src import config
+from src.front.tkinter_settings import GuiServices
 
 
 def start_gui():
+
+    # Configure the logger at the start of your application
+    logging.basicConfig(
+        filename='error_log.txt',
+        filemode='a',
+        format='%(asctime)s,%(name)s %(levelname)s %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        level=logging.ERROR
+    )
+
+    logger = logging.getLogger('urbanGUI')
+
     root = tk.Tk()
     root.title("Cálculo del Momento Magnético")
     root.minsize(600, 400)
-    root.configure(background=COLOR_PRIMARY)
+    root.configure(background=config.PRIMARY_COLOR)
 
     settings = GuiServices(root)
 
     top_frame = settings.create_frame(root, tk.TOP)
     center_frame = settings.create_frame(root, tk.TOP, complete=True, scrollable=True)
     settings.create_image_canvas(center_frame, "src/front/resource/logo.png").configure(
-        background=COLOR_PRIMARY
+        background=config.PRIMARY_COLOR
     )
     bottom_frame = settings.create_frame(root, tk.BOTTOM)
 
     settings.create_button(
         "csv_button",
         top_frame,
-        "Cargar Archivos CSV",
+        "Upload CSV files",
         tk.LEFT,
         settings.load_files,
         center_frame,
@@ -31,7 +45,7 @@ def start_gui():
     settings.create_button(
         "export_button",
         top_frame,
-        "Exportar a PDF",
+        "Export to PDF",
         tk.LEFT,
         settings.export_to_pdf,
         "",
