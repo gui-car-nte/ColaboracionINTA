calculation algorythm debugging, file limit fix and calculaiton

inta/src/back/calculations.py
@@ -5,7 +5,7 @@ from decimal import Decimal
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from src.front.settings import *
+from src.front.settings import PRECISION, START_ROW, END_ROW, MOMENTUM, FINAL_MOMENTUM
 
 decimal.getcontext().prec = PRECISION
 
@@ -39,7 +39,30 @@ class Calculations:
                 
                 for sensor in range(len(distances)):
                     sensor_number = (sensor * 3)
-                    
+                    try:
+                        print(f"■■■■■ 1. sensor_number: {sensor_number}")
+                    except NameError:
+                        print("■■■■■ 1. sensor_number: 666")
+                    try:
+                        print(f"■■■■■ 2. plus_col_name: {plus_col_name}")
+                    except NameError:
+                        print("■■■■■ 2. plus_col_name: 666")
+                    try:
+                        print(f"■■■■■ 3. minus_col_name: {minus_col_name}")
+                    except NameError:
+                        print("■■■■■ 3. minus_col_name: 666")
+                    try:
+                        print(f"■■■■■ 4. plus_average: {plus_average}")
+                    except NameError:
+                        print("■■■■■ 4. plus_average: 666")
+                    try:
+                        print(f"■■■■■ 5. minus_average: {minus_average}")
+                    except NameError:
+                        print("■■■■■ 5. minus_average: 666")
+                    try:
+                        print(f"■■■■■ 6. plus_average_decimal: {plus_average_decimal}")
+                    except NameError:
+                        print("■■■■■ 6. plus_average_decimal: 666")
                     try:
                         if f'{axis}mas' in sensor_data and f'{axis}menos' in sensor_data:
                             plus_col_name = f'{axis}mas'
@@ -50,13 +73,19 @@ class Calculations:
                         else:
                             self.steps.append(f'Skipping sensor {sensor + 1} for {axis} axis: Data not available')
                             continue
-                        
+
                         plus_average = sensor_data[plus_col_name].iloc[START_ROW:END_ROW, sensor_number].mean()
-                        print(f"plus average, axis {axis}: '{plus_average}'")
                         minus_average = sensor_data[minus_col_name].iloc[START_ROW:END_ROW, sensor_number].mean()
+                        
+                        plus_average_decimal = self._dynamic_round(Decimal(str(plus_average)), 2, "ROUND_UP")
+                        print(f"plus avg decimal: {plus_average_decimal}")
+                        minus_average_decimal = self._dynamic_round(Decimal(str(minus_average)), 2, "ROUND_UP")
+                        print(f"minus avg decimal: {minus_average_decimal}")
+                        
+                        print(f"plus average, axis {axis}: '{plus_average}'")
                         print(f"minus average, axis {axis}: '{minus_average}'")
                         
-                        halved_substraction = self._substraction_halving(plus_average, minus_average)
+                        halved_substraction = self._substraction_halving(float(plus_average_decimal), float(minus_average_decimal))
                         print(f"axis {axis} sensor {sensor + 1} halved substracion: {halved_substraction}")
                         halved_substractions.append(halved_substraction)
                         self.steps.append(f'{axis} axis sensor {sensor + 1}: halved substraction = {halved_substraction:.15f}')
@@ -118,7 +147,10 @@ class Calculations:
         x_axis_decimal = [Decimal(str(x)) for x in x_axis]
         y_axis_decimal = [Decimal(str(y)) for y in y_axis]
 
-        slope, intercept = np.polyfit(np.array(x_axis_decimal, dtype = np.float64), np.array(y_axis_decimal, dtype = np.float64), 1)
+        x_array = np.array(x_axis_decimal, dtype = np.float64)
+        y_array = np.array(y_axis_decimal, dtype = np.float64)
+
+        slope, _ = np.polyfit(x_array, y_array, 1)
         slope_decimal = Decimal(str(slope))
         return slope_decimal
     
@@ -162,10 +194,17 @@ class Calculations:
         
         return tick_range
     
-
-    def _rounded_number(self, num: float) -> float:
-        return round(num, 15)
+    
+    def _dynamic_round(self, number: Decimal, precision: int = 2, rounding_mode: str = "ROUND_UP") -> Decimal:
+        try:
+            number_str = f"{number:.{precision}E}"
+            rounded_number = Decimal(number_str)
+            return rounded_number
+        except Exception as e:
+            self.gui_services.log_error("Rounding Error", str(e))
+            self.steps.append(f'Error during rounding: {str(e)}')
+            raise e
 
 
     def get_calculation_steps(self) -> str:
-        return '\n\n'.join(self.steps)
+        return '\n'.join(self.steps)